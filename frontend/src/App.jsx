353fab3577d0603db3010de77ecf16630be08c91
Rahul Kumar Tiwari <irktiwari@gmail.com>
2025-08-22 07:16:57 +0530

    made the navbar section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom'
 
-function App() {
+const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
-    <>
-      <div></div>
-    </>
-  );
+    <div>
+      { !isOwnerPath && <Navbar /> }
+    </div>
+  )
 }
 
-export default App;
+export default App
