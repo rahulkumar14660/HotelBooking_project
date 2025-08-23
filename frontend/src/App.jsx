fc9e402e28a4260787ea2608277dacfd95ba10bd
Rahul Kumar Tiwari <irktiwari@gmail.com>
2025-08-23 14:50:26 +0530

    added listed page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
 
 const App = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={ <Home /> } />
+          <Route path='/rooms' element={ <AllRooms /> } />
         </Routes>
       </div>
 
