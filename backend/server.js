8d5ba7e1795ef0d004cde6c1d4926ef5b49f53e4
Rahul Kumar Tiwari <irktiwari@gmail.com>
2025-08-25 15:41:25 +0530

    created api for hotels

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,33 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import roomRouter from ".routes/roomRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 connectDB();
+connectCloudinary();
 
 const app = express();
-app.use(cors());
+app.use(cors());                // Enable Cross-Origin Resource Sharing
+
+// Middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// API to listen to Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res) => {
-    res.send("API is working fine");
+    res.send("API is working");
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/hotels', hotelRouter);
+app.use('/api/rooms', roomRouter);
 
 const PORT = process.env.PORT || 3000;
 
