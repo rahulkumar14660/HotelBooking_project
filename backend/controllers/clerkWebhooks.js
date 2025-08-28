1182494dca616df6be4ccb44872d5a7645ee0888
Rahul Kumar Tiwari <irktiwari@gmail.com>
2025-08-28 18:16:56 +0530

    project completed

--- backend/controllers/clerkWebhooks.js
+++ backend/controllers/clerkWebhooks.js
@@ -17,23 +17,32 @@ const clerkWebhooks = async(req, res) => {
         await whook.verify(JSON.stringify(req.body), headers)
 
         // Getting data from request body
-        const { data, type } = req.body
-
-        const userData = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name + " " + data.last_name,
-            image: data.image_url,
-        }
+        const { data, type } = req.body;
 
         // Switch Cases for different Events
         switch (type) {
             case "user.created": {
+
+                const userData = {
+                _id: data.id,
+                email: data.email_addresses[0].email_address,
+                username: data.first_name + " " + data.last_name,
+                image: data.image_url,
+            }
+
                 await User.create(userData);
                 break;
             }
 
             case "user.updated": {
+                
+                const userData = {
+                _id: data.id,
+                email: data.email_addresses[0].email_address,
+                username: data.first_name + " " + data.last_name,
+                image: data.image_url,
+            }
+
                 await User.findByIdAndUpdate(data.id, userData);
                 break;
             }
