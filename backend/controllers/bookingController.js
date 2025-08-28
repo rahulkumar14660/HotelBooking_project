5a85375b72a27449555fb46b908b4026f96207ef
Rahul Kumar Tiwari <irktiwari@gmail.com>
2025-08-28 18:13:38 +0530

    added booking confirmation through email

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,3 +1,4 @@
+import transporter from "../configs/nodemailer.js";
 import Booking from "../models/Booking.js";
 import Hotel from "../models/Hotel.js";
 import Room from "../models/Room.js";
@@ -12,7 +13,7 @@ const checkAvailability = async({ checkInDate, checkOutDate, room }) => {
             checkOutDate: { $gte: checkInDate },
         });
         const isAvailable = bookings.length === 0;
-        return isAvailable
+        return isAvailable;
     }
     catch(error) {
         console.error(error.message);
@@ -74,6 +75,29 @@ export const createBooking = async(req, res) => {
             checkOutDate,
             totalPrice,
         });
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: req.user.email,
+            subject: 'Hotel Booking Details',
+            html: `
+                <h2> Your Booking Details </h2>
+                <p> Dear ${req.user.username}, </p
+                <p> Thank you for booking! Here are your details: </p>
+                <ul>
+                    <li> <strong>Booking ID:</strong> ${booking._id} </li>
+                    <li> <strong>Hotel Name:</strong> ${roomData.hotel.name} </li>
+                    <li> <strong>Location:</strong> ${roomData.hotel.address} </li>
+                    <li> <strong>Date:</strong> ${booking.checkInDate.toDateString()} </li>
+                    <li> <strong>Booking Amount:</strong> ${process.env.CURRENCY || '$'} ${booking.totalPrice} /night </li>
+                </ul>
+
+                <p> We look forward to welcoming you! </p>
+                <p> If you need make any changes, feel free to contact us. </p>
+            `
+        }
+
+        await transporter.sendMail(mailOptions);
 
         res.json({ success: true, message: "Booking created successfully" });
 
